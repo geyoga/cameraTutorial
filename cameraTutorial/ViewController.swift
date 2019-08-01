20594b215c2eda7d7fd203b36439968918a87fe0
geyoga <yogadewantama@gmail.com>
2019-08-01 15:45:19 +0700

    Initial Commit

---
+++ cameraTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cameraTutorial
+//
+//  Created by Georgius Yoga Dewantama on 01/08/19.
+//  Copyright © 2019 Georgius Yoga Dewantama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
